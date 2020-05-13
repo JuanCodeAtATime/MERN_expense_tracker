5da02a0f1c145a3ec962311797f1bd79198d9829
Juan Rivera <jrivera@primalplatoon.com>
2020-05-13 19:39:07 -0400

    .

--- server.js
+++ server.js
@@ -22,12 +22,13 @@ if (process.env.NODE_ENV === 'production') {
 app.use('/api/v1/transactions', tranactions);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static("client/build"));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
+
 
 const PORT = process.env.PORT || 5000
 
