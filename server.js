d469d2a1f4186c4ed211c6388e022909e1177e6e
Juan Rivera <jrivera@primalplatoon.com>
2020-05-13 20:07:27 -0400

    .

--- server.js
+++ server.js
@@ -24,9 +24,10 @@ app.use('/api/v1/transactions', tranactions);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static("client/build"));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    })
+    app.get('*', function (req, res) {
+        const index = path.join(__dirname, 'build', 'index.html');
+        res.sendFile(index);
+    });
 }
 
 
